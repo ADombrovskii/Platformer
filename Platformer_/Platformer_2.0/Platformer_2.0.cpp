f10862fae59db3f9b61ac4eca76120a35b4ad951
ADombrovskii <adom3d55@gmail.com>
2021-02-26 20:09:33 +0600

    теперь падение инрока в функци, плюс в x_generator 2 while а не 3, а самое главное сейчас игрок не падает на платформах и сделано это без массива

--- Platformer_/Platformer_2.0/Platformer_2.0.cpp
+++ Platformer_/Platformer_2.0/Platformer_2.0.cpp
@@ -13,14 +13,18 @@ using namespace std;
 random_device rd;
 mt19937 gen(rd());
 
-int width = 80;
-int height = 50;
+const int width = 80;
+int height = 20;
 
 int x_cursor;
 int y_cursor;
 
-int y_player = 0;
-int x_player = 0;
+int y_player = 10;
+int x_player = 3;
+
+int kol_vo_platform = 15;
+
+//int a = x_platforms.size();
 
 COORD makeCoord(int x, int y) {
 	COORD coord = { (SHORT)x, (SHORT)y };
@@ -42,45 +46,40 @@ vector<int>x_platforms_width;
 vector<int>y_platforms;
 vector<int>x_platforms;
 
-void y_generator() 
+void y_generator()
 {
 	uniform_int_distribution<> ot_do(2, 4);
-	for (int i = 0; i < 15; i++)
+	for (int i = 0; i < kol_vo_platform; i++)
 	{
 		y_platforms.push_back(ot_do(gen));
+		y_platforms[i] = height - y_platforms[i] - 2;
 	}
-}  
+}
 
 void x_generator()
 {
 	int a = x_platforms.size();
 	uniform_int_distribution<> ot_do(3, 5);
-	for (int i = 0; i < 15; i++) // ширина платформ от 3 до 5 
+	for (int i = 0; i < kol_vo_platform; i++) // ширина платформ от 3 до 5 
 	{
 		x_platforms_width.push_back(ot_do(gen));
+		x_platforms.push_back(i);
 	}
 	int o = 0;
-	while (o < 15) // создание мссива каординат платформ из 15 штук 
-	{
-		x_platforms.push_back(o);
-		o++;
-	}
-	o = 0;
 	x_platforms[o] = o; // 0 создание каординаты на которой будет находится 1 платформа
-	o++;
-	while (o < 15)
+	while (o + 1 < kol_vo_platform)
 	{
-		if (x_platforms[o] + x_platforms_width[o] > width)
+		if (x_platforms[o + 1] + x_platforms_width[o + 1] > width)
 		{
 			break;
 		}
 		else
 		{
-			x_platforms[o] = x_platforms[o - 1] + x_platforms_width[o - 1] + 2;
+			x_platforms[o + 1] = x_platforms[o - 1 + 1] + x_platforms_width[o - 1 + 1] + 2;
 			o++;
 		}
 	}
-} 
+}
 
 void log()
 {
@@ -98,19 +97,12 @@ void log()
 		{
 		case 'w': // сравниваем с w
 			y_player--;
-			cout << "x_player = " << x_player << " y_player = " << y_player;
 			break;
 		case 'a': // сравниваем с a
 			x_player--;
-			cout << "x_player = " << x_player << " y_player = " << y_player;
 			break;
 		case 'd': // сравниваем с d
 			x_player++;
-			cout << "x_player = " << x_player << " y_player = " << y_player;
-			break;
-		case 's': // сравниваем с s
-			y_player++;
-			cout << "x_player = " << x_player << " y_player = " << y_player;
 			break;
 		}
 	if (x_player == width - 2 || y_player == height - 2 || x_player == -1 || y_player == -1)
@@ -121,10 +113,29 @@ void log()
 	z = true;
 }
 
+void padenie_igroka()
+{
+	bool h = false;
+	for (int i = 0; i < kol_vo_platform; i++)
+	{
+		tek_time = time();
+		if (x_player >= x_platforms[i] && x_player < x_platforms_width[i] + x_platforms[i] && y_player + 1 == y_platforms[i])
+		{
+			h = true;
+		}
+	}
+	if (tek_time > vrem_poden && h == false)
+	{
+		vrem_poden = vrem_poden + 300;
+		y_player++;
+		h = false;
+	}
+}
+
 void draw_platforms()
 {
 	int a = 0;
-	while (a < 15)
+	while (a < kol_vo_platform)
 	{
 		if (y_cursor == y_platforms[a] && x_cursor == x_platforms[a])
 		{
@@ -178,21 +189,20 @@ void draw()
 	}
 	x_cursor = 0;
 	y_cursor = 0;
+	cout << "x_player = " << x_player << " y_player = " << y_player;
+	fflush(stdout);
 }
 
 int main()
 {
+	char buf[width / 4];
+	setvbuf(stdout, buf, _IOFBF, sizeof(buf));
 	y_generator();
 	x_generator();
 	while (1)
 	{
-		tek_time = time();
 		log();
 		draw();
-		if (tek_time > vrem_poden)
-		{
-			vrem_poden = vrem_poden + 300;
-			y_player++;
-		}
+		padenie_igroka();
 	}
 }
