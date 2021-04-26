b12105606fb7de6bfefc6f8f3f52d1fbef95423f
Sasha <adom3d55@gmail.com>
2021-04-26 21:00:51 +0300

    теперь можно еще удобнее рисовать платформы

--- Platformer_/editor/editor.cpp
+++ Platformer_/editor/editor.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+
 #define _USE_MATH_DEFINES
 
 using namespace std;
@@ -22,6 +23,8 @@ int height = 20;
 
 int x_cursor = 0;
 int y_cursor = 0;
+
+int do_x_platform_width;
 
 struct Platform
 {
@@ -37,71 +40,99 @@ HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 bool a = false;
 
+int _platform_width;
+
 void editor(HANDLE hStdin, bool o)
 {
-		const short bufferSize = 128;
-		INPUT_RECORD IrInBuf[bufferSize];
-		DWORD cNumRead = 0;
-		BOOL peekSuccessful = PeekConsoleInput(hStdin, IrInBuf, bufferSize, &cNumRead);
-		if (cNumRead > 0) {
-			ReadConsoleInput(hStdin, IrInBuf, bufferSize, &cNumRead);
-			//cout << cNumRead << "\n";
-			MOUSE_EVENT_RECORD Event_Mouse;
-			for (DWORD i = 0; i < cNumRead; i++)
+	const short bufferSize = 128;
+	INPUT_RECORD IrInBuf[bufferSize];
+	DWORD cNumRead = 0;
+	BOOL peekSuccessful = PeekConsoleInput(hStdin, IrInBuf, bufferSize, &cNumRead);
+	if (cNumRead > 0) {
+		ReadConsoleInput(hStdin, IrInBuf, bufferSize, &cNumRead);
+		//cout << cNumRead << "\n";
+		MOUSE_EVENT_RECORD Event_Mouse;
+		for (DWORD i = 0; i < cNumRead; i++)
+		{
+			if (IrInBuf[i].EventType == MOUSE_EVENT)
 			{
-				if (IrInBuf[i].EventType == MOUSE_EVENT)
+				Event_Mouse = IrInBuf[i].Event.MouseEvent;
+				if (Event_Mouse.dwEventFlags == 0 && Event_Mouse.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
 				{
-					Event_Mouse = IrInBuf[i].Event.MouseEvent;
-					if (Event_Mouse.dwEventFlags == 0 && Event_Mouse.dwButtonState == FROM_LEFT_1ST_BUTTON_PRESSED)
-					{
-						cout << "x_platforms = " << platform.x << " y_platforms = " << platform.y << endl;
-						platform.x = Event_Mouse.dwMousePosition.X; ////////////////////////////
-						platform.y = Event_Mouse.dwMousePosition.Y; ////////////////////////////
-						a = true;
-					}
-					if (Event_Mouse.dwEventFlags == MOUSE_MOVED) // ПРОВЕРЯЕТ СВЯЗАНО СОБЫТИЕ ЛИ С ДВИЖЕНИЕМ
-					{
-						if (a == true)
-						{
-							platform.xw = Event_Mouse.dwMousePosition.X - platform.x + 1;
-							platforms.push_back(platform);
-						}
-					}
-					if (Event_Mouse.dwEventFlags == 0 && Event_Mouse.dwButtonState == 0)
-					{
-						//platform.xw = Event_Mouse.dwMousePosition.X - platform.x + 1;
-						//platforms.push_back(platform);
-						cout << "x_platforms_width = " << platform.xw << endl;
-						cout << "Нажата левая кнопка мыши\n";
-
-						if (b == 0)
-						{
-							out << platforms[platforms.size() - 1].x;
-							out << endl << platforms[platforms.size() - 1].xw;
-							out << endl << platforms[platforms.size() - 1].y;
-							out.flush();
-							b = 1;
-						}
-						else
-						{
-							out << endl << platforms[platforms.size() - 1].x;
-							out << endl << platforms[platforms.size() - 1].xw;
-							out << endl << platforms[platforms.size() - 1].y;
-							out.flush();
-						}
-						a = false;
-						//cout << "Вы отпустили мышь";
-					}
-					//if (Event_Mouse.dwEventFlags == DOUBLE_CLICK)
-					//{
-					//	cout << "Двойное нажатие левой кнопки мыши\n";
-					//}
+					platform.x = Event_Mouse.dwMousePosition.X;
+					platform.y = Event_Mouse.dwMousePosition.Y;
+					a = true;
 				}
+				if (Event_Mouse.dwEventFlags == MOUSE_MOVED) // ПРОВЕРЯЕТ СВЯЗАНО СОБЫТИЕ ЛИ С ДВИЖЕНИЕМ
+				{
+					if (a == true)
+					{
+						do_x_platform_width = platform.xw;
+						platform.xw = Event_Mouse.dwMousePosition.X - platform.x + 1;
+						platforms.push_back(platform);
+						_platform_width = platform.x - Event_Mouse.dwMousePosition.X;
+						if (_platform_width < 0)
+						{
+							_platform_width = 0;
+						}
+					}
+				}
+				if (Event_Mouse.dwEventFlags == 0 && Event_Mouse.dwButtonState == 0)
+				{
+					//platform.xw = Event_Mouse.dwMousePosition.X - platform.x + 1;
+					//platforms.push_back(platform);
+
+					/*cout << "x_platforms = " << platform.x << " y_platforms = " << platform.y << endl;
+					cout << "x_platforms_width = " << platform.xw;
+
+					if (b == 0)
+					{
+						out << platforms[platforms.size() - 1].x;
+						out << endl << platforms[platforms.size() - 1].xw;
+						out << endl << platforms[platforms.size() - 1].y;
+						out.flush();
+						b = 1;
+					}
+					else
+					{
+						out << endl << platforms[platforms.size() - 1].x;
+						out << endl << platforms[platforms.size() - 1].xw;
+						out << endl << platforms[platforms.size() - 1].y;
+						out.flush();
+					}*/
+					cout << "x_platforms = " << platform.x << " y_platforms = " << platform.y << endl;
+					cout << "x_platforms_width = " << platform.xw << endl;
+					cout << "x_platforms = " << platform.x - abs(_platform_width) << " y_platforms = " << platform.y << endl;
+					cout << "x_platforms_width = " << platform.xw + abs(_platform_width);
+
+					if (b == 0)
+					{
+						out << platforms[platforms.size() - 1].x - abs(_platform_width);
+						out << endl << platforms[platforms.size() - 1].xw + abs(_platform_width);
+						out << endl << platforms[platforms.size() - 1].y;
+						out.flush();
+						b = 1;
+					}
+					else
+					{
+						out << endl << platforms[platforms.size() - 1].x - abs(_platform_width);
+						out << endl << platforms[platforms.size() - 1].xw + abs(_platform_width);
+						out << endl << platforms[platforms.size() - 1].y;
+						out.flush();
+					}
+					a = false;
+					//cout << "Вы отпустили мышь";
+				}
+				//if (Event_Mouse.dwEventFlags == DOUBLE_CLICK)
+				//{
+				//	cout << "Двойное нажатие левой кнопки мыши\n";
+				//}
 			}
 		}
+	}
 }
 
-void draw_platforms(Platform a)
+void draw_platforms(Platform a, int b, int c)
 {
 	for (int i = 0; i < a.xw; i++)
 	{
@@ -110,7 +141,21 @@ void draw_platforms(Platform a)
 		cout << '%';
 		fflush(stdout);
 	}
-	COORD position = { 0, 21 }; //позиция x и y
+	for (int i = 0; i < b; i++)
+	{
+		COORD position = { short(a.x - i), short(a.y) }; //позиция x и y
+		SetConsoleCursorPosition(hConsole, position);
+		cout << '%';
+		fflush(stdout);
+	}
+	for (int i = 0; i < c - a.xw; i++)
+	{
+		COORD position = { short(a.x + a.xw + i), short(a.y) }; //позиция x и y
+		SetConsoleCursorPosition(hConsole, position);
+		cout << ' ';
+		fflush(stdout);
+	}
+	COORD position = { 0, 20 }; //позиция x и y
 	SetConsoleCursorPosition(hConsole, position);
 }
 
@@ -169,7 +214,7 @@ int main()
 		}
 		else
 		{
-			draw_platforms(platforms[platforms.size() - 1]);
+			draw_platforms(platforms[platforms.size() - 1], _platform_width, do_x_platform_width);
 			p = true;
 		}
 	}
